e036d411bdf0f8a48e456caf35558c5200552894
Noura <nouraaaa108@gmail.com>
2026-06-22 18:45:55 +0400

    feat : create a responsive Hero section.

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+
+function Hero() {
+  return (
+       <section className='bg-indigo-700 py-20 mb-4 '>
+        <div className='flex flex-col items-center '>
+            <div className="flex  flex-col items-center gap-2">
+                 <h1 className='text-white text-3xl font-extrabold md:text-4xl'>Become a React Dev</h1>
+                <p className='text-white text-sm md:text-xl '>Find the React job that fits your skills and needs</p> 
+            </div>
+        </div>
+
+       </section>
+
+  )
+}
+
+export default Hero
