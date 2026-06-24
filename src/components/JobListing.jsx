4ad1ac3eec4297a849662696853226d2b7c8b0a3
Noura <nouramursal@gmail.com>
2026-06-24 15:52:59 +0400

    refactor(routing): replace anchor tag with React Router Link

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,6 @@
 import { FaMapMarker } from "react-icons/fa";
 import { useState } from "react"
-
+import { Link } from "react-router-dom";
 function JobListing({job}) {
     const [showJobDescription, setShowJobDescription] = useState(false);
     let description = job.description;
@@ -29,12 +29,12 @@ function JobListing({job}) {
                 <FaMapMarker className="inline text-lg mb-1 mr-1"/> {job.location}
 
                 </div>
-                <a
-                  href={`/job/${job.id}`}
+                <Link
+                  to={`/job/${job.id}`}
                   className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                  Read More
-                </a>
+                </Link>
               </div>
             </div>
           </div>
