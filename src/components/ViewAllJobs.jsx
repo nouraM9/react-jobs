3c7ab34f40c8543f80cbac2760fffbebf9527bf5
Noura <nouramursal@gmail.com>
2026-07-01 14:51:16 +0400

    feat : add view all jobs button component.

--- src/components/ViewAllJobs.jsx
+++ src/components/ViewAllJobs.jsx
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 
 function ViewAllJobs() {
   return (
-  <section className="m-auto max-w-lg my-10 px-6">
+    <section className="m-auto max-w-lg my-10 px-6">
       <Link
         to="/jobs"
         className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs ></Link>/>
+      >
+        View All Jobs &gt;
+      </Link>
     </section>
-)
+  );
 }
 
-export default ViewAllJobs
+export default ViewAllJobs;
