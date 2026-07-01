481cf67f494ac102502351cb3bf026dd92c7a9d7
Noura <nouramursal@gmail.com>
2026-07-01 14:42:49 +0400

    feat : add error page route.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import {Route , createBrowserRouter, createRoutesFromElements,RouterProvider} fr
 
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
-
+import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage';
 const router = createBrowserRouter(createRoutesFromElements(
 <Route path='/' element={<MainLayout />}>
-
-<Route index element ={<HomePage />}/>
+  <Route index element ={<HomePage isHome = {true} />}/>
+  <Route path="/jobs" element = {<JobsPage />} />
+  <Route path='*' element = {<NotFoundPage />}/>
 
 </Route>
 
