3024f50080a5a6082bbda1934c5fb464d1da657f
Noura <nouramursal@gmail.com>
2026-07-09 21:14:24 +0400

    feat : add job route and single job loader.

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,20 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage';
+import SingleJob,{jobLoader} from './pages/SingleJob';
+import AddJob from './pages/AddJob';
+
 const router = createBrowserRouter(createRoutesFromElements(
 <Route path='/' element={<MainLayout />}>
   <Route index element ={<HomePage isHome = {true} />}/>
   <Route path="/jobs" element = {<JobsPage />} />
-  <Route path='*' element = {<NotFoundPage />}/>
+  <Route
+        path="jobs/:id"
+        element={<SingleJob />}
+        loader={jobLoader}
+      />  
+      <Route path='/add-job' element = {<AddJob/>}></Route>
+      <Route path='*' element = {<NotFoundPage />}/>
 
 </Route>
 
