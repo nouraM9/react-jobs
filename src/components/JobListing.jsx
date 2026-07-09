283ef41e600675517127a418e2c4728ca6590267
Noura <nouramursal@gmail.com>
2026-07-09 21:23:56 +0400

    fix : fix the the link url.

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,7 @@
 import { FaMapMarker } from "react-icons/fa";
 import { useState } from "react"
 import { Link } from "react-router-dom";
+
 function JobListing({job}) {
     const [showJobDescription, setShowJobDescription] = useState(false);
     let description = job.description;
@@ -30,7 +31,7 @@ function JobListing({job}) {
 
                 </div>
                 <Link
-                  to={`/job/${job.id}`}
+                  to={`/jobs/${job.id}`}
                   className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                  Read More
