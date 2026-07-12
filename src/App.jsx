379ef919862d454a8119a981b0bb1bdc6daf5362
Noura <nouramursal@gmail.com>
2026-07-12 11:35:02 +0400

    feat : impliment add new job function.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,23 @@ import NotFoundPage from './pages/NotFoundPage';
 import SingleJob,{jobLoader} from './pages/SingleJob';
 import AddJob from './pages/AddJob';
 
+
+
+function App() {
+  // Add New Job
+const addJob = async (newJob) =>{
+  const res = await fetch('api/jobs',{
+    method : 'POST',
+    headers : {
+      'Content-Type' : 'application/json',
+    },
+    body : JSON.stringify(newJob)
+  });
+  return;
+}
+
+// Delete Job
+
 const router = createBrowserRouter(createRoutesFromElements(
 <Route path='/' element={<MainLayout />}>
   <Route index element ={<HomePage isHome = {true} />}/>
@@ -16,13 +33,13 @@ const router = createBrowserRouter(createRoutesFromElements(
         element={<SingleJob />}
         loader={jobLoader}
       />  
-      <Route path='/add-job' element = {<AddJob/>}></Route>
+      <Route path='/add-job'   element={<AddJob addJobSubmit={addJob} />} 
+ ></Route>
       <Route path='*' element = {<NotFoundPage />}/>
 
 </Route>
 
 ))
-function App() {
   return <RouterProvider router ={router} />
 };
 
