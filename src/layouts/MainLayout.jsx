1a7eb2b94df1de80ecdf157db8746002f6cb8948
Noura <nouramursal@gmail.com>
2026-07-12 14:21:01 +0400

    feat : add toast npm package for notification.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function Mainlayout() {
   return (
 <>
 <Navbar />
 <Outlet />
+<ToastContainer />
+
 
 </>
 )
